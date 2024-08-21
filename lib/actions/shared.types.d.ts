ff981ced85dca3ffbacf38fbbb60e87407182feb
Pritam Ghosh <pri10145@gmail.com>
2024-08-21 12:18:36 +0530

    webhook

--- lib/actions/shared.types.d.ts
+++ lib/actions/shared.types.d.ts
@@ -1,17 +1,35 @@
-import { IUser } from "@/models/user.model"
-import { Schema } from "mongoose"
+import { Schema } from "mongoose";
+import { IUser } from "@/models/user.model";
 
 export interface createQuestionParams {
-    title: string
-    content: string
-    tags: string[]
-    author: Schema.Types.ObjectId | IUser
-    path: string
+  title: string;
+  content: string;
+  tags: string[];
+  author: Schema.Types.ObjectId | IUser;
+  path: string;
 }
 
 export interface GetQuestionsParams {
-    page?: number;
-    pageSize?: number;
-    searchQuery?: string;
-    filter?: string;
-  }
+  page?: number;
+  pageSize?: number;
+  searchQuery?: string;
+  filter?: string;
+}
+
+export interface CreateUserParams {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  picture: string;
+}
+
+export interface UpdateUserParams {
+  clerkId: string;
+  updateData: Partial<IUser>;
+  path: string;
+}
+
+export interface DeleteUserParams {
+  clerkId: string;
+}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,7 +1,14 @@
-'use server'
+"use server";
 
 import User from "@/models/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/models/question.model";
 
 export async function getUserById(params: any) {
   try {
@@ -16,7 +23,57 @@ export async function getUserById(params: any) {
     return user;
   } catch (error) {
     console.log(error);
-    
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    await Question.deleteMany({ author: user._id });
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+
+    return deletedUser;
+  } catch (error) {
+    console.log(error);
     throw error;
   }
 }
