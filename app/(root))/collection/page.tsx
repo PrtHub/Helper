cf76b2cf325f78e8c76c19ad5b75d01ed4cbbd86
Pritam Ghosh <pri10145@gmail.com>
2024-08-30 12:21:48 +0530

    collection page

---
+++ app/(root))/collection/page.tsx
@@ -0,0 +1,64 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import LocalSearchbar from "@/components/search/LocalSearchbar";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { QuestionFilters } from "@/lib/constant";
+import { auth } from "@clerk/nextjs/server";
+import React from "react";
+
+const page = async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const result = await getSavedQuestions({
+    clerkId: userId,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark-100 dark:text-light-900">
+        Saved Questions
+      </h1>
+      <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+
+        <Filter
+          filters={QuestionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </section>
+
+      <section className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ?
+          result.questions.map((question: any) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+          : <NoResult
+            title="There’s no question saved to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />}
+      </section>
+    </>
+  );
+};
+
+export default page;
