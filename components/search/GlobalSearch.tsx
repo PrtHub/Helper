a78a2cf4cc7f8cde0e839a8d1c0b90cd6a816c7d
Pritam <pri10145@gmail.com>
2024-09-15 15:38:07 +0530

    global search

--- components/search/GlobalSearch.tsx
+++ components/search/GlobalSearch.tsx
@@ -1,19 +1,90 @@
-import { Search } from 'lucide-react'
-import { Input } from '@/components/ui/input'
+"use client";
+
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import GlobalResult from "./GlobalResult";
 
 const GlobalSearch = () => {
-  return (
-    <section className='w-full relative max-w-[500px] max-lg:hidden'>
-       <div className='relative bg-light-800 dark:dark-gradient flex items-center gap-1 grow px-4 rounded-xl min-h-12'>
-        <Search className='cursor-pointer text-light-500'/>
-        <Input
-          type='text'
-          placeholder='Search for anything...'
-          className="paragraph-regular text-light-400 no-focus placeholder bg-transparent border-none shadow-none outline-none"
-         />
-       </div>
-    </section>
-  )
-}
+  const searchContainRef = useRef(null);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
 
-export default GlobalSearch
+  const query = searchParams.get("q");
+
+  const [search, setSearch] = useState(query || "");
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: any) => {
+      if (
+        searchContainRef.current &&
+        // @ts-ignore
+        !searchContainRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+        setSearch("");
+      }
+    };
+
+    setIsOpen(false);
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [pathname]);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "global",
+          value: search,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (query) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["global", "type"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, searchParams, router, query, pathname]);
+
+  return (
+    <section
+      className="w-full relative max-w-[500px] max-lg:hidden"
+      ref={searchContainRef}
+    >
+      <div className="relative bg-light-800 dark:dark-gradient flex items-center gap-1 grow px-4 rounded-xl min-h-12">
+        <Search className="cursor-pointer text-light-500" />
+        <Input
+          type="text"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            if (!isOpen) setIsOpen(true);
+            if (e.target.value === "" && isOpen) setIsOpen(false);
+          }}
+          placeholder="Search for anything..."
+          className="paragraph-regular text-light-400 no-focus placeholder bg-transparent border-none shadow-none outline-none"
+        />
+      </div>
+      {isOpen && <GlobalResult />}
+    </section>
+  );
+};
+
+export default GlobalSearch;
--- lib/actions/shared.types.d.ts
+++ lib/actions/shared.types.d.ts
@@ -142,3 +142,8 @@ export interface DeleteAnswerParams {
   answerId: string;
   path: string;
 }
+
+export interface SearchParams {
+  query?: string | null;
+  type?: string | null;
+}
